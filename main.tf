f8d4840993e9f2a26b4640dd394d1832dc366703
Terraform User <builder-admin@example.com>
2026-02-03 21:23:47 +0000

    Managed by Terraform

--- main.tf
+++ main.tf
@@ -2,101 +2,47 @@ locals {
   istio_charts_url = "https://istio-release.storage.googleapis.com/charts"
 }
 
-# resource "kubernetes_namespace" "istio-system" {
-#   metadata {
-#     # annotations = {
-#     #   name = "istio-system"
-#     # }
-
-#     # labels = {
-#     #   mylabel = "istio-system"
-#     # }
-
-#     name = "istio-system"
-#   }
-# }
 
 resource "helm_release" "istio-base" {
   repository       = local.istio_charts_url
-  chart            = "istio/base"
+  chart            = "base"
   name             = "istio-base"
   namespace        = "istio-system"
   version          = var.istio_version
   create_namespace = true
-  depends_on       = [kubernetes_namespace.istio-system]
 
-  # set {
-  #   name  = "cni.enabled"
-  #   value = "true"
-  # }
-}
-
-data "http" "manifestfile" {
-  url = "https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.4.0/experimental-install.yaml"
+  set {
+    name  = "defaultRevision"
+    value = "default"
+  }
 }
 
 
-resource "kubernetes_manifest" "kubernetes_gateway_API_crd" {
-  manifest = data.manifestfile.body
-}
-
-
-# resource "helm_release" "istiod" {
+# resource "helm_release" "istio-cniagent" {
 #   repository       = local.istio_charts_url
-#   chart            = "istiod"
-#   name             = "istiod"
+#   chart            = "cni"
+#   name             = "istio-cni"
 #   namespace        = "istio-system"
-#   create_namespace = false
 #   version          = var.istio_version
-#   depends_on       = [helm_release.istio-base, kubernetes_namespace.istio-system]
-# }
-
-# resource "helm_release" "istio-cni" {
-#   repository = local.istio_charts_url
-#   chart      = "gateway"
-#   name       = "istio-cni"
-#   namespace  = "istio-system"
-#   version    = var.istio_version
-#   depends_on = [helm_release.istiod]
-# }
-
-# resource "kubernetes_namespace" "istio-ingress" {
-#   metadata {
-#     labels = {
-#       istio-injection = "enabled"
-#     }
-
-#     name = "istio-ingress"
-#   }
+#   create_namespace = true
 # }
 
 
-# resource "kubernetes_namespace" "istio-egress" {
-#   metadata {
-#     labels = {
-#       istio-injection = "enabled"
-#     }
+resource "helm_release" "istio-ingress" {
+  repository       = local.istio_charts_url
+  chart            = "gateway"
+  name             = "istio-ingress"
+  namespace        = "istio-ingress"
+  version          = var.istio_version
+  create_namespace = true
+}
 
-#     name = "istio-egress"
-#   }
-# }
-
-# resource "helm_release" "istio-ingress" {
-#   repository       = local.istio_charts_url
-#   chart            = "gateway"
-#   name             = "istio-ingress"
-#   namespace        = "istio-ingress"
-#   create_namespace = false
-#   version          = var.istio_version
-#   depends_on       = [helm_release.istiod, kubernetes_namespace.istio-ingress]
-# }
 
 # resource "helm_release" "istio-egress" {
 #   repository       = local.istio_charts_url
 #   chart            = "gateway"
 #   name             = "istio-egress"
 #   namespace        = "istio-egress"
-#   create_namespace = false
 #   version          = var.istio_version
-#   depends_on       = [helm_release.istiod, kubernetes_namespace.istio-egress]
+#   create_namespace = true
 # }
